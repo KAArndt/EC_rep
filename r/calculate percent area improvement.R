fed3da8ba4eb7d8f13400cdc137c8dfb9a08ad88
KAArndt <kyleaarndt@gmail.com>
2024-11-18 20:35:46 +0000

    edits

--- r/calculate percent area improvement.R
+++ r/calculate percent area improvement.R
@@ -13,24 +13,31 @@ names(improve) = 'improve'
 all = c(base,improve,clust)
 
 df = as.data.frame(x = all)
+summary(df$base.dist)
+er1 = 1.9
+er4 = 1.44
 
-df$base.er1 = ifelse(df$base.dist <= 1.54,1,0)
-df$base.er4 = ifelse(df$base.dist <= 1.27,1,0)
+df$base.er1 = ifelse(df$base.dist <= er1,1,0)
+df$base.er4 = ifelse(df$base.dist <= er4,1,0)
 
-df$new.er1 = ifelse(df$improve <= 1.54,1,0)
-df$new.er4 = ifelse(df$improve <= 1.27,1,0)
+df$new.er1 = ifelse(df$improve <= er1,1,0)
+df$new.er4 = ifelse(df$improve <= er4,1,0)
 
 df$all = 1
 
-df$change = df$base.dist -df$improve
+df$change = df$base.dist - df$improve
 df$change.count = ifelse(df$change > 0 ,1,0)
+sum(df$change)
+
 
 summary = df %>%
   summarise(base.er1 = sum(base.er1)/sum(all),
-            base.er4 = sum(base.er4)/sum(all),
             new.er1  = sum(new.er1)/sum(all),
+            base.er4 = sum(base.er4)/sum(all),
             new.er4  = sum(new.er4)/sum(all),
-            change = sum(change.count)/sum(all))
+            change   = sum(change.count)/sum(all),
+            total    = (sum(base.dist) - sum(improve))/sum(base.dist)
+            )
 
 
 summary(df$improve)
